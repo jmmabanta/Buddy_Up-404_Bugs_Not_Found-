ee23fdfa57ee7c2a314d15720d46dd1b7c347d70
John Mabanta <johnmabanta51@gmail.com>
2023-01-07 14:06:31 -0700

    Progress

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
+const fileUpload = require("express-fileupload");
 const cors = require("cors");
 const app = express();
 const port = 3000;
 
 const test = require("./routes/test");
+const schedule_parse = require("./routes/schedule_parse");
 
 app.use(cors({ origin: true }));
+app.use(
+  fileUpload({
+    safeFileNames: true,
+  })
+);
 
 app.use("/api", test);
+app.use("/api/schedule", schedule_parse);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
