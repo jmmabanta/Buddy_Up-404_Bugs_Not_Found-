2faadd11ab379fa1ed55942b83acca681491f746
John Mabanta <johnmabanta51@gmail.com>
2023-01-07 21:37:49 -0700

    Backend login

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { OAuth2Client } = require("google-auth-library");
 const router = express.Router();
+const { getUserData } = require("../util.js");
+const { OAuth2Client } = require("google-auth-library");
+
+const authClient = new OAuth2Client(
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET,
+  "postmessage" // For some reason, this MUST be "postmessage"
+);
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://db:27017/users");
@@ -20,20 +27,10 @@ const User = new mongoose.model("User", UserSchema);
 
 const jsonParser = bodyParser.json();
 
-const authClient = new OAuth2Client(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
-  "postmessage" // For some reason, this MUST be "postmessage"
-);
-
 // Verify login and return user info
 router.post("/", jsonParser, async (req, res) => {
   const { tokens } = await authClient.getToken(req.body.code);
-  const ticket = await authClient.verifyIdToken({
-    idToken: tokens.id_token,
-    audience: process.env.GOOGLE_CLIENT_ID,
-  });
-  const payload = ticket.getPayload();
+  const payload = await getUserData(tokens.id_token);
   User.findOrCreate(
     {
       googleID: payload["sub"],
