3548c06e171ae57bccba5082c7bbe846c988a655
John Mabanta <johnmabanta51@gmail.com>
2023-01-07 21:26:04 -0700

    Database works

--- backend/src/routes/login.js
+++ backend/src/routes/login.js
@@ -3,6 +3,21 @@ const bodyParser = require("body-parser");
 const { OAuth2Client } = require("google-auth-library");
 const router = express.Router();
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://db:27017/users");
+
+const findOrCreate = require("mongoose-findorcreate");
+const UserSchema = new mongoose.Schema({
+  googleID: String,
+  name: String,
+  email: String,
+  picture: String,
+  faculty: String,
+  courses: [{ name: String, type: String, section: String }],
+});
+UserSchema.plugin(findOrCreate);
+const User = new mongoose.model("User", UserSchema);
+
 const jsonParser = bodyParser.json();
 
 const authClient = new OAuth2Client(
@@ -19,6 +34,16 @@ router.post("/", jsonParser, async (req, res) => {
     audience: process.env.GOOGLE_CLIENT_ID,
   });
   const payload = ticket.getPayload();
+  User.findOrCreate(
+    {
+      googleID: payload["sub"],
+    },
+    {
+      name: payload["name"],
+      email: payload["email"],
+      picture: payload["picture"],
+    }
+  );
   res.json(payload);
 });
 
